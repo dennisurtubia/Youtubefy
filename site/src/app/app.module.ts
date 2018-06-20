d8fec5b7558364f40231ba9618cfafb7fcbf81ce
Willian Rodrigues <oredstonebr@gmail.com>
2018-06-20 16:46:03 -0300

    criado telas no angular

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -1,16 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
+import { HomeComponent } from "./home/home.component";
+import { PublisherComponent } from './publisher/publisher.component';
+import { AdminComponent } from './admin/admin.component';
+import { PlayerComponent } from './player/player.component';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { AlbumComponent } from './album/album.component';
 
-import { AppComponent } from './app.component';
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    PageNotFoundComponent,
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    PublisherComponent,
+    AdminComponent,
+    PlayerComponent,
+    PlaylistComponent,
+    AlbumComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
