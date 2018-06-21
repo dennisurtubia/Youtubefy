4539d07779e5cd62a9830fca6c013ba2f5f4d0da
Willian Rodrigues <oredstonebr@gmail.com>
2018-06-21 17:23:22 -0300

    Adiciona capa de playlist interativa

--- site/src/app/playlist/playlist.component.ts
+++ site/src/app/playlist/playlist.component.ts
@@ -8,14 +8,19 @@ import { LocalStorageService, SessionStorageService } from "ngx-webstorage";
 })
 export class PlaylistComponent implements OnInit {
   currPlaylist: any[];
+  image: string;
   constructor(
     private localSt: LocalStorageService,
     private sessionSt: SessionStorageService
-  ) {}
+  ) {
+    this.image =
+      "https://lh5.googleusercontent.com/-444l_KCN5iA/TXFDWWJyuwI/AAAAAAAABNY/PLNJLdqMyg4/s320/Pink_Floyd_-_Dark_Side_of_the_Moon.jpg";
+  }
   changePlaylist() {}
   ngOnInit() {
     this.currPlaylist = this.localSt.retrieve("currPlaylist");
   }
+
   ngOnDestroy() {
     this.localSt.clear("currPlaylist");
   }
