f6ca237781e9c39c442be523804d68cdadbe2412
Willian <willianbarbosa@alunos.utfpr.edu.br>
2018-06-24 23:51:44 -0300

    Implementado adição de album pela publicadora

--- site/src/app/publisher/publisher.component.ts
+++ site/src/app/publisher/publisher.component.ts
@@ -1,4 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService, SessionStorageService } from "ngx-webstorage";
+import { GetApiService } from '../get-api.service';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Router} from "@angular/router";
+import {Musica} from "../musica";
+
+interface Object{
+  nome?:string;
+  senha?:string;
+  cpf?:number;
+  email?:string;
+  token?:string;
+  id?:number;
+  generos?:any[];
+  cnpj?:number;
+  capa?:string;
+  descricao?:string;
+  nomeArtista?:string;
+  explicito?:boolean;
+  url?:string;
+  genero?:number;
+  duracao?:number;
+}
+
 
 @Component({
   selector: 'app-publisher',
@@ -6,10 +30,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./publisher.component.css']
 })
 export class PublisherComponent implements OnInit {
-
-  constructor() { }
-
+  form:FormGroup;
+  musica: FormGroup;
+  musicas: Array<Musica> =[];
+  
+  constructor(
+    private localSt: LocalStorageService,
+    private fb: FormBuilder,
+    private sessionSt: SessionStorageService,
+    private getApi:GetApiService,
+    private router: Router
+  ) {
+    
+    this.createForm();
+  }
+  addAlbum(){
+    this.getApi.postAlbum(this.form.value, this.musicas);
+  }
+  addMusic(){
+    this.musicas.push({
+      'nome':this.musica.value.nome,
+      'duracao':this.musica.value.duracao,
+      'url':this.musica.value.url,
+      'explicito':this.musica.value.explicito,
+      'genero':this.musica.value.genero
+    });
+    if (this.musica.valid) {
+      console.log("Form Submitted!");
+      this.musica.reset();
+    }
+    // this.musicas[length].nome = this.musica.value.nome;
+    // this.musicas[length].duracao = this.musica.value.duracao;
+    // this.musicas[length].explicito = this.musica.value.explicito;
+    // this.musicas[length].genero = this.musica.value.genero;
+    // this.musicas[length].url = this.musica.value.url;
+  }
+  createForm() {
+    this.form = this.fb.group({
+      nome: [ '', Validators.required ],
+      nomeArtista: [ '', Validators.required],
+      capa: ['', Validators.required],
+      descricao: ['', Validators.required],
+    });
+    this.musica = this.fb.group({
+      nome: [ '', Validators.required ],
+      duracao: [ '', Validators.required],
+      explicito: ['', Validators.required],
+      genero: ['', Validators.required],
+      url: ['', Validators.required],
+    });
+  }
   ngOnInit() {
+
+    if(this.localSt.retrieve('token') === null) {
+      this.router.navigate(['/login']);
+    }
   }
 
 }
