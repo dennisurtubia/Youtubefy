305c98460abbf14da899e2b74979808efdbd667e
Jorge Rossi <jorgefzrossi@gmail.com>
2018-06-27 18:46:50 -0300

    Datapath: componentes e suas interligações que possibilitam a execução das instruções de um processador

--- api/app/controllers/PlaylistController.ts
+++ api/app/controllers/PlaylistController.ts
@@ -1,3 +1,46 @@
+import { Authorized, CurrentUser, JsonController, Body, Put } from "routing-controllers";
+import PlaylistPrivadaRepository from "../repositories/PlaylistPublicaRepository"
+import { IsNotEmpty, IsString } from "class-validator";
+
+class InsertRequest {
+
+    @IsString()
+    @IsNotEmpty()
+    nome: string = "";
+    
+}
+@JsonController("/playlist")
+export default class PlaylistController {
+    PlaylistPrivadaRepository: any;
+
+    @Authorized("OUVINTE")
+    @Put("/")
+    async update1(
+        @CurrentUser({ required: true }) email: string,
+        @Body({ validate: true }) req: InsertRequest
+    ){
+        const playlistPrivada = await this.PlaylistPrivadaRepository.getByEmail(email);
+        if(playlistPrivada === null)
+            return;
+
+        playlistPrivada.nome = req.nome;
+        return{"sucesso": true};
+    }
+    @Authorized("ADMIN")
+    @Put("/")
+    async update2(
+        @CurrentUser({ required: true }) email: string,
+        @Body({ validate: true }) req: InsertRequest
+    ){
+        const playlistPrivada = await this.PlaylistPrivadaRepository.getByEmail(email);
+        if(playlistPrivada === null)
+            return;
+
+        playlistPrivada.nome = req.nome;
+        return{"sucesso": true};
+    }
+}
+
 // TODO:
 /*
 
